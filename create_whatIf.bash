ebc4329f5be4681f9923ae0225c036046814ff75
Alex H <alexhuft@gmail.com>
2022-07-25 11:40:45 -0700

    added script to automate adding new problems

---
+++ create_whatIf.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Author:      Alex Huft
+# Date:        25-07-2022 (July 25th, 2022)
+# Description: create new directory structure for a 'what if problem'
+
+new_problem=$1
+problem_solvers=("Alex" "Diego")
+
+
+if [ -d $new_problem ]; then
+    echo "${new_problem} already exists"
+    exit 1
+fi
+
+/bin/mkdir $new_problem
+
+pushd $new_problem >> /dev/null
+
+for user in ${problem_solvers[@]}; do
+    /bin/mkdir $user
+    pushd $user >> /dev/null
+    /bin/touch main.ipynb
+    popd >> /dev/null
+done
